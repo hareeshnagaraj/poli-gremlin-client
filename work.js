9cb0b4fcf2618d5b1f0cc782515121a60fb6cde1
Mike <mahathaway93@gmail.com>
2017-07-08 23:18:57 -0700

    modern variable declarations

--- work.js
+++ work.js
@@ -1,4 +1,4 @@
-// Set your API key to an environment variable 
+// Set your API key to an environment variable
 // Below script assumes propublicakey=yourkey and is accessed through process.env
 // You can set the env var with the following command: export propublicakey={your key}
 
@@ -8,12 +8,12 @@ const Congress = require('propublica-congress-node');
 const uuid = require('uuid');
 
 // Global members
-var client = new Congress(process.env.propublicakey);
-var globalWorkDir;
+const client = new Congress(process.env.propublicakey);
+let globalWorkDir;
 
-var initializeWorkDirectory = function()
+const initializeWorkDirectory = function()
 {
-	var workDir = process.cwd() + '/work/';
+	let workDir = process.cwd() + '/work/';
 
 	if (!fs.existsSync(workDir)){
 		fs.mkdirSync(workDir);
@@ -31,9 +31,8 @@ var initializeWorkDirectory = function()
 
 // Retrieve information for a given congress session
 // 102-115 for House, 80-115 for Senate
-var populateCongressMembers = function(congressSeshNumber, chamber, workDir)
+const populateCongressMembers = function(congressSeshNumber, chamber, workDir)
 {
-	var resp;
 
 	if(chamber != 'house' && chamber != 'senate')
 	{
@@ -44,42 +43,43 @@ var populateCongressMembers = function(congressSeshNumber, chamber, workDir)
 		congressNumber: congressSeshNumber,
 		chamber: chamber
 	}).then(function(res, error){
-		var data = res['results'][0];
+		const data = res['results'][0];
 		// Write JSON file with all members for this particular congress
 		if(workDir != null)
 		{
 			writeResponseFile(data, 'CongressMembers_'+congressSeshNumber+'_'+chamber, workDir)
 		}
 
-		var members = data['members'];
+		const members = data['members'];
 		// Enumerate the response
 		Promise.map(members, populateMemberInfo);
 	});
 };
 
-var populateMemberInfo = function(x)
+const populateMemberInfo = function(x)
 {
 	client.memberBioAndRoles({
 		memberId: x['id']
 	}).then(function(res, error){
-		var data = res['results'][0];
-		var apiName = 'Member_' + x['id'] + '_' + x['first_name'] + x['last_name'] + '_' + uuid.v1();
+		const data = res['results'][0];
+		const apiName = 'Member_' + x['id'] + '_' + x['first_name'] + x['last_name'] + '_' + uuid.v1();
 		writeResponseFile(data, apiName, globalWorkDir);
-	});
+	})
+	.catch((err => console.error(err)))
 }
 
 // Write a file with the JSON response from ProPublica API
-var writeResponseFile = function(data, apiname, dir)
-{	
-	var fileName = dir + '/' + apiname + '_' + uuid.v1() + '.json';
+const writeResponseFile = function(data, apiname, dir)
+{
+	const fileName = dir + '/' + apiname + '_' + uuid.v1() + '.json';
 	console.log('Writing ' + fileName);
 
-	var writeStream = fs.createWriteStream(fileName);
+	const writeStream = fs.createWriteStream(fileName);
 	writeStream.write(JSON.stringify(data));
 	writeStream.end();
 }
 
 // Testing
-var globalWorkDir = initializeWorkDirectory();
+globalWorkDir = initializeWorkDirectory();
 populateCongressMembers(115, 'house', globalWorkDir);
 populateCongressMembers(115, 'senate', globalWorkDir);
