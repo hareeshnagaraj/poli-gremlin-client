6c91806ef8ea2f8d74a5be78d8ef52eb199a31a8
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2017-07-16 16:10:51 -0700

    Query strings added

--- app.js
+++ app.js
@@ -1,21 +1,93 @@
 // Populate a JanusGraph server with data pulled from ProPublica API
 // Repo: https://github.com/hareeshnagaraj/propublica-api-population
 
-// Assuming Node.js or Browser environment with browserify: 
-var gremlin = require('gremlin-client');
- 
-// Will open a WebSocket to ws://localhost:8182 by default 
-var client = gremlin.createClient();
+// Process command line arguments
+const argv = require('minimist')(process.argv.slice(2));
 
-var query = client.stream('g.V()');
+const fs = require('fs');
+const gremlin = require('gremlin-client');
+const Promise = require('bluebird');
+
+const args = argv['_'];
+const populate = 'populate';
+const house = 'house';
+const senate = 'senate';
+
+// Will open a WebSocket to ws://localhost:8182 by default 
+const client = gremlin.createClient();
+
+const PopulateSenateMembers = function(jsonFilePath){
+    var json = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+    var congressNum = json['congress'];
+    var senateMembers = json['members'];
+    
+    for(var i = 0; i < senateMembers.length; i++){
+        // Client operation on given member
+        var currentMember = senateMembers[i];
+        var firstName = currentMember['first_name'];
+        var lastName = currentMember['last_name'];
+        var vertexName = firstName + '-' + lastName;
+
+        var queryStr = '';
+        for(var key in currentMember){
+            queryStr += ("\"" + key + "\"" + ',' + "\"" + currentMember[key] + "\"" + ',');
+        }
+        
+        queryStr = queryStr.slice(0, -1);
+
+        var clientQueryStr = 'graph.addVertex(' + queryStr + ');';
+        console.log(clientQueryStr);
+    }
+}
+
+const AddSenateMemberVertex = function(x)
+{
+
+}
+
+// var vertexQuery = client.stream('g.V()');
  
-// If playing with classic TinkerPop graph, will emit 6 data events 
-query.on('data', function(result) {
-  // Handle first vertex 
-  console.log(result);
-  console.log(result['properties']);
-});
+// vertexQuery.on('data', function(result) {
+//   console.log(result);
+//   removeVertices(result['properties']);
+// });
  
-query.on('end', function() {
-  console.log("All results fetched");
-});
+// vertexQuery.on('end', function() {
+//   console.log("All results fetched");
+// });
+
+// function removeVertices(nodes)
+// {
+//   console.log(nodes);
+// }
+
+// 2 graphs
+// 1 senate, 1 house
+// Start w/senate for ease of use
+
+
+// Command line parsing
+if(args[0] == populate){
+
+    if(args.length < 3)
+    {
+        throw new Error('Incorrect usage. Ex: node app.js populate [house/senate] [path to json]');
+    }
+    else if (args[1] != house && args[1] != senate)
+    {
+        throw new Error('Incorrect usage. Ex: node app.js populate [house/senate] [path to json]');        
+    }
+
+    var chamber = args[1];
+    var file = args[2];
+
+    if (!fs.existsSync(file)) {
+        throw new Error('Could not find file : ' + file);        
+    }
+
+    if (chamber == senate){
+        PopulateSenateMembers(file);
+    }
+
+    console.log('Populating ' + chamber + ' from : ' + file);
+}
