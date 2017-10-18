c72cd5c1b48cd016f07a4844b6e4c5464619761f
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2017-10-18 00:48:16 -0700

    NIT

--- graphui/src/components/App.tsx
+++ graphui/src/components/App.tsx
@@ -10,8 +10,19 @@ import '../styles/App.css';
 const gremlin = require('gremlin-client');
 const GremlinClient = gremlin.createClient(8182,'40.112.250.222');
 
-let nodeA:string = "122925224";
-let nodeB:string = "40980536";
+//let bernieSanders:string = "122925224";
+let angusKing:string = "40980536";
+let initGraphGremlinQuery = 'g.V()';
+
+// Additional queries to be processed
+// g.E().has('disagreevotes',gt('5')).values('disagreevotes')
+// g.E().has('agreePercent', gt('20'))
+// g.V('122925224').outE()
+// g.V('122925224').outE().has('agreePercent', gt('50'))
+
+interface GremlinCallback {
+    ( e: any, results: any ) : void;
+}
 
 interface Props {
   width: number;
@@ -39,7 +50,8 @@ export default class App extends React.Component<Props, {}> {
   }
   
   componentDidMount(){
-    this.initializeGraph(nodeA,nodeB);
+    //this.initializeGraph(bernieSanders);
+    this.initializeGraph(angusKing)
   }
 
   componentWillReceiveProps(nextProps:Props) {
@@ -52,24 +64,6 @@ export default class App extends React.Component<Props, {}> {
     let firstName = decodeURIComponent(node.properties.first_name[0].value);
     let lastName = decodeURIComponent(node.properties.last_name[0].value);
     return `${firstName} ${lastName}`;
-  }
-
-  drawGraph_(){
-      this.simulation = d3.forceSimulation()
-      .force("link", 
-        d3.forceLink()
-          .id((d: d3Types.d3Node) => {
-            return d.id;
-          })
-          .distance((d: d3Types.d3Link)=>{
-            return d.value;
-        }))
-      .force("charge", d3.forceManyBody().strength(-1000))
-      .force("center", d3.forceCenter(this.width / 2, this.height / 2))
-      .nodes(this.graph.nodes);
-
-    this.simulation.force("link").links(this.graph.links);
-    d3.selectAll(".node").on("click",this.clicked.bind(this));
   }
 
   drawGraph = () => {
@@ -86,42 +80,49 @@ export default class App extends React.Component<Props, {}> {
       this.simulation.nodes(this.graph.nodes).on("tick", this.ticked);
       d3.selectAll(".node").on("click",this.clicked.bind(this));
   
+      this.simulation.alphaTarget(0.05);
       this.simulation.restart();
       console.log("Restarted sim");
   }
 
-  initializeGraph = (id1:string, id2:string) => {
-    let d3graph:d3Types.d3Graph = { nodes: [], links: []};
-    this.inMemoryData = { inMemoryNodes : {}, inMemoryLinks : {} };
+  initializeGraph = (id1:string) => {
+    let newGraph:d3Types.d3Graph = { nodes: [], links: []};
+    // this.inMemoryData = { inMemoryNodes : {}, inMemoryLinks : {} };
     
-    GremlinClient.execute(
-      "g.V()",
-      {},
-      (e: any, results: any) => {
-          d3graph.nodes = this.createD3JsonNodes(results);
-          this.addNodeEdges(d3graph, id1);
+    this.gremlin(initGraphGremlinQuery, {}, (e: any, results: any) => {
+          newGraph.nodes = this.createD3JsonNodes(results);
+          this.addAllOutgoingNodeEdges(newGraph, id1);
           this.throwOnError(e);
       });
   }
 
-  addNodeEdges = (d3graph:d3Types.d3Graph, id1:string) => {
-
-    GremlinClient.execute(
-      `g.V(${id1}).outE()`,
+  addAllOutgoingNodeEdges = (newGraph:d3Types.d3Graph, nodeId:string) => {
+    this.gremlin(
+      `g.V(${nodeId}).outE()`,
       {},
+      (e:any, results:any) => {
+        newGraph.links = newGraph.links.concat(this.createD3JsonLinks(newGraph.nodes, results));
+
+        this.graph = newGraph;
+        this.setState((prevState, props) => {
+          return {
+            graph: this.graph
+          };
+        }, 
+      this.drawGraph);
+    });
+  }
+
+  gremlin = (query:string, bindings:any, callback:GremlinCallback) => {
+    GremlinClient.execute(
+      query,
+      bindings,
       (e: any, results: any) => {
 
         this.throwOnError(e);
-        d3graph.links = (this.createD3JsonLinks(d3graph.nodes, results));
-        this.graph = d3graph;
 
         try{
-          this.setState((prevState, props) => {
-                return {
-                  graph: this.graph
-                };
-              }, 
-              this.drawGraph);
+          callback(e, results);
         }catch(any){
           console.log(any);
         }
@@ -148,6 +149,7 @@ export default class App extends React.Component<Props, {}> {
 
         let name = this.getName(node);
         node.index = i;
+        node.updated = false;
         this.inMemoryData.inMemoryNodes[node.id] = node;
         this.idMap[name] = node.id;
 
@@ -197,8 +199,8 @@ export default class App extends React.Component<Props, {}> {
   }
 
   clicked(x:any){
-       this.addNodeEdges(this.graph, x.details.id);
-   }
+      this.addAllOutgoingNodeEdges(this.graph, x.details.id);
+  }
 
   ticked() {
      const node = d3.selectAll(".node");
