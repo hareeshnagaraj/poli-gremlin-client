72a68fad93910e0a455048edba5ae13c5be09c09
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2017-10-22 19:22:06 -0400

    index changes

--- graphui/src/index.tsx
+++ graphui/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './components/App';
+import Graph from './components/App';
 import './styles/index.css';
 
 let d3graph:any = {};
@@ -8,7 +8,7 @@ d3graph.nodes = [];
 d3graph.links = [];
 
 ReactDOM.render(
-  <App
+  <Graph
     width={window.screen.availWidth}
     height={window.screen.availHeight}
     graph={d3graph} />,
